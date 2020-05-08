d8112959aae694c368aed003936c1099b7f9e88f
indrnoken <61928913+indrnoken@users.noreply.github.com>
2020-05-08 13:48:37 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pholyrism
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            printer printer;
+
+            Console.WriteLine("\t| jenis printer | ");
+            Console.WriteLine(" 1. canon");
+            Console.WriteLine("2.epson");
+            Console.WriteLine("3.laserjet");
+            Console.Write("piliha printer (1..3) :");
+
+            int pilihprinter = Convert.ToInt32(Console.ReadLine());
+
+            if (pilihprinter == 1)
+                printer = new canon();
+            else if (pilihprinter == 2)
+                printer = new epson();
+            else
+                printer = new laserjet();
+        }
+    }
+}
---
+++ printer.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pholyrism
+{
+    class printer
+    {
+        public virtual void show()
+        {
+            Console.WriteLine("  ");
+        }
+        public virtual void print()
+        {
+            Console.WriteLine(" ");
+        }
+
+    }
+
+    class canon : printer
+    {
+        public override void show()
+        {
+            base.show();
+            Console.Write(" canon display dimension is 9.5 x 12");
+        }
+        public override void print()
+        {
+            base.print();
+            Console.Write("canon printing.....");
+        }
+    }
+        class epson : printer
+        {
+            public override void show()
+            {
+                base.show();
+                Console.Write("epson display dimension : 10 x 11");
+            }
+            public override void print()
+            {
+                base.print();
+                Console.Write(" epson printing ......");
+            }
+
+        }
+        class laserjet : printer
+        {
+            public override void show()
+            {
+                base.show();
+                Console.Write(" laserjet dimension : 12 x 12");
+            }
+            public override void print()
+            {
+                base.print();
+                Console.Write(" laserjet is priting .....");
+            }
+        }
+    }
+
+
